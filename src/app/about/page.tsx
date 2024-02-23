38e0159f0359848d55ca3ba017c3904ce00e4b40
Sayan Das <sayan17102002@gmail.com>
2024-02-23 12:57:46 +0530

    about and contact page updated

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,9 +1,37 @@
-
-
-export default function About(){
-    return(
-        <>
-            <h1>This is about page</h1>
-        </>
-    )
-}
+export default function About() {
+    return (
+      <div className="bg-cover bg-center min-h-75 h-auto w-full py-2 px-10 md:px-30 flex flex-wrap justify-start items-center">
+        <div className="max-w-800 flex flex-col items-center py-10 px-10 md:py-40 md:px-40 bg-red-100 rounded-lg ">
+          <p className="text-2xl md:font-extrabold font-bold leading-14 md:text-3xl md:leading-11 text-center mb-12">About Us</p>
+          <p className="text-xl leading-8 md:text-center text-start mb-24 md:text-base md:leading-6">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu</p>
+          <div className="flex justify-center">
+            <a className="mr-5 hover:cursor-pointer">
+              <img
+                className="w-6 h-6 md:w-8 md:h-8"
+                src="https://workik-widget-assets.s3.amazonaws.com/widget-assets/images/bb33.png"
+              />
+            </a>
+            <a className="mr-5 hover:cursor-pointer">
+              <img
+                className="w-6 h-6 md:w-8 md:h-8"
+                src="https://workik-widget-assets.s3.amazonaws.com/widget-assets/images/bb34.png"
+              />
+            </a>
+            <a className="mr-5 hover:cursor-pointer">
+              <img
+                className="w-6 h-6 md:w-8 md:h-8"
+                src="https://workik-widget-assets.s3.amazonaws.com/widget-assets/images/bb35.png"
+              />
+            </a>
+            <a className="hover:cursor-pointer">
+              <img
+                className="w-6 h-6 md:w-8 md:h-8"
+                src="https://workik-widget-assets.s3.amazonaws.com/widget-assets/images/bb36.png"
+              />
+            </a>
+          </div>
+        </div>
+      </div>
+    );
+  }
+  
--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,9 +1,50 @@
-
-
-export default function Contact(){
-    return(
-        <>
-            <h1>This is contact page</h1>
-        </>
-    )
+export default function Contact() {
+  return (
+    <div className="bg-cover bg-center min-h-75 h-auto w-full py-2 px-10 md:px-30 flex flex-wrap justify-start items-center">
+    <div className="max-w-800 flex flex-col items-center md:py-20 md:px-20 bg-red-100 rounded-lg">
+      <div className="text-center w-full">
+        <h1 className="text-4xl lg:text-6xl">Contact Form</h1>
+      </div>
+      <div className="max-w-screen-xl mt-16 px-8 grid gap-8 grid-cols-1 md:grid-cols-2 md:px-12 lg:px-16 xl:px-32 py-16 mx-auto bg-gray-100 text-gray-900 rounded-lg shadow-lg">
+        <div className="flex flex-col justify-between">
+            <h2 className="text-4xl lg:text-5xl font-semibold leading-tight">
+              Lets talk about everything!
+            </h2>
+        </div>
+        <div className="">
+          <div>
+            <span className="uppercase text-sm text-gray-600 font-bold">
+              Full Name
+            </span>
+            <input
+              className="w-full bg-gray-300 text-gray-900 mt-2 p-3 rounded-lg focus:outline-none focus:shadow-outline"
+              type="text"
+              placeholder=""
+            />
+          </div>
+          <div className="mt-8">
+            <span className="uppercase text-sm text-gray-600 font-bold">
+              Email
+            </span>
+            <input
+              className="w-full bg-gray-300 text-gray-900 mt-2 p-3 rounded-lg focus:outline-none focus:shadow-outline"
+              type="text"
+            />
+          </div>
+          <div className="mt-8">
+            <span className="uppercase text-sm text-gray-600 font-bold">
+              Message
+            </span>
+            <textarea className="w-full h-32 bg-gray-300 text-gray-900 mt-2 p-3 rounded-lg focus:outline-none focus:shadow-outline"></textarea>
+          </div>
+          <div className="mt-8">
+            <button className="uppercase text-sm font-bold tracking-wide bg-indigo-500 text-gray-100 p-3 rounded-lg w-full focus:outline-none focus:shadow-outline">
+              Send Message
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+    </div>
+  );
 }
