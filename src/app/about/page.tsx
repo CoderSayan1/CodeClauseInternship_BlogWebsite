25664afb0f5f00f0f4aeb2d950c4be1adfc8144f
Sayan Das <sayan17102002@gmail.com>
2024-02-24 21:03:40 +0530

    about link added

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 export default function About() {
     return (
       <div className="bg-cover bg-center min-h-75 h-auto w-full py-2 px-10 md:px-30 flex flex-wrap justify-start items-center">
@@ -5,34 +7,34 @@ export default function About() {
           <p className="text-2xl md:font-extrabold font-bold leading-14 md:text-3xl md:leading-11 text-center mb-12">About Us</p>
           <p className="text-xl leading-8 md:text-center text-start mb-24 md:text-base md:leading-6">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu</p>
           <div className="flex justify-center">
-            <a className="mr-5 hover:cursor-pointer">
+            <Link href={"https://www.facebook.com"} target="_blank" className="mr-5 hover:cursor-pointer">
               <img
                 className="w-6 h-6 md:w-8 md:h-8"
                 src="https://workik-widget-assets.s3.amazonaws.com/widget-assets/images/bb33.png"
                 alt="Reload it"
               />
-            </a>
-            <a className="mr-5 hover:cursor-pointer">
+            </Link>
+            <Link href={"https://www.linkedin.com/in/sayan-das-179b93226/"} target="_blank" className="mr-5 hover:cursor-pointer">
               <img
                 className="w-6 h-6 md:w-8 md:h-8"
                 src="https://workik-widget-assets.s3.amazonaws.com/widget-assets/images/bb34.png"
                 alt="Reload it"
               />
-            </a>
-            <a className="mr-5 hover:cursor-pointer">
+            </Link>
+            <Link href={"https://www.instagram.com"} target="_blank" className="mr-5 hover:cursor-pointer">
               <img
                 className="w-6 h-6 md:w-8 md:h-8"
                 src="https://workik-widget-assets.s3.amazonaws.com/widget-assets/images/bb35.png"
                 alt="Reload it"
               />
-            </a>
-            <a className="hover:cursor-pointer">
+            </Link>
+            <Link href={"https://www.twitter.com"} target="_blank" className="hover:cursor-pointer">
               <img
                 className="w-6 h-6 md:w-8 md:h-8"
                 src="https://workik-widget-assets.s3.amazonaws.com/widget-assets/images/bb36.png"
                 alt="Reload it"
               />
-            </a>
+            </Link>
           </div>
         </div>
       </div>
